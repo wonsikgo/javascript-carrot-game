008a2ce6e27f9bce99364647c43b0947793c6ec9
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-04-11 13:49:40 +0900

    사운드 경로 수정

--- src/sound.js
+++ src/sound.js
@@ -1,8 +1,8 @@
-const bgm = new Audio("../static/sound/bg.mp3");
-const gameLoseSound = new Audio("../static/sound/alert.wav");
-const gameWinSound = new Audio("../static/sound/game_win.mp3");
-const carrotPullSound = new Audio("../static/sound/carrot_pull.mp3");
-const bugPullSound = new Audio("../static/sound/bug_pull.mp3");
+const bgm = new Audio("./static/sound/bg.mp3");
+const gameLoseSound = new Audio("./static/sound/alert.wav");
+const gameWinSound = new Audio("./static/sound/game_win.mp3");
+const carrotPullSound = new Audio("./static/sound/carrot_pull.mp3");
+const bugPullSound = new Audio("./static/sound/bug_pull.mp3");
 
 export function playBgm() {
   playSound(bgm);
